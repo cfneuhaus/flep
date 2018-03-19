dae0812cd94f9c8861fd99385011b9680c8bdaf3
Frank Neuhaus <fneuhaus@uni-koblenz.de>
2018-03-19 17:32:41 +0100

    up

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(name='flep',
       version='1.0',
-      py_modules=['flep'],
       author='Frank Neuhaus',
       author_email='dtag00@gmail.com',
-      install_requires=['matplotlib', 'networkx']
+      install_requires=['matplotlib', 'networkx'],
+      scripts=['bin/flep']
       )
